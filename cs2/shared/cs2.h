2cca323e967920ef2f9aabea768b4ecfba47184e
BaldCult <112122534+BaldCult@users.noreply.github.com>
2024-03-20 18:43:09 -0400

    Update cs2.h

--- cs2/shared/cs2.h
+++ cs2/shared/cs2.h
@@ -16,7 +16,7 @@
 #ifndef _KERNEL_MODE
 #include <stdio.h>
 #ifdef DEBUG
-#define LOG(...) printf("[baldhook.win] " __VA_ARGS__)
+#define LOG(...) printf("[EC] " __VA_ARGS__)
 #else
 #define LOG(...) // __VA_ARGS__
 #endif
@@ -24,7 +24,7 @@
 #else
 
 #ifdef DEBUG
-#define LOG(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[baldhook.win] " __VA_ARGS__)
+#define LOG(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[EC] " __VA_ARGS__)
 #else
 #define LOG(...) // __VA_ARGS__
 #endif
